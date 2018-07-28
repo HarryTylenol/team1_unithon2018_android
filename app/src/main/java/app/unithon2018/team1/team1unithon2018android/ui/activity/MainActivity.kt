63d5d9530f0a0331641248f1b87cdcad1fb95eb6
박현기 <jungsanmango@gmail.com>
2018-07-28 15:34:55 +0900

    Added Google Map API Key

--- app/src/main/java/app/unithon2018/team1/team1unithon2018android/ui/activity/MainActivity.kt
+++ app/src/main/java/app/unithon2018/team1/team1unithon2018android/ui/activity/MainActivity.kt
@@ -6,18 +6,36 @@ import app.unithon2018.team1.team1unithon2018android.R
 import app.unithon2018.team1.team1unithon2018android.data.PostRepository
 import app.unithon2018.team1.team1unithon2018android.data.RoomRepository
 import app.unithon2018.team1.team1unithon2018android.network.ApiManager
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
   private val apiService = ApiManager.getApiService()
 
   private val roomRepository by lazy { RoomRepository.instance(apiService) }
   private val postRepository by lazy { PostRepository.instance(apiService) }
 
+  lateinit var map: GoogleMap
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
+    val mapFragment = supportFragmentManager
+        .findFragmentById(R.id.map) as SupportMapFragment
+    mapFragment.getMapAsync(this)
   }
 
+  override fun onMapReady(googleMap: GoogleMap) {
+    map = googleMap
+
+    val sydney = LatLng(-34.0, 151.0)
+    map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+    map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+  }
 }
