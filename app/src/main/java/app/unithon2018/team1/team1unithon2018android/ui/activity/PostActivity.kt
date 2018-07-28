258f506aa0d338e36b7d0f08bbad631f96b7c0a3
byungtak <>
2018-07-28 19:15:10 +0900

    adding post, room activiry wire frame

--- app/src/main/java/app/unithon2018/team1/team1unithon2018android/ui/activity/PostActivity.kt
+++ app/src/main/java/app/unithon2018/team1/team1unithon2018android/ui/activity/PostActivity.kt
@@ -6,6 +6,12 @@ import app.unithon2018.team1.team1unithon2018android.R
 import app.unithon2018.team1.team1unithon2018android.data.PostRepository
 import app.unithon2018.team1.team1unithon2018android.data.RoomRepository
 import app.unithon2018.team1.team1unithon2018android.network.ApiManager
+import android.widget.Toast
+import android.R.menu
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+
 
 class PostActivity : AppCompatActivity() {
 
@@ -17,5 +23,24 @@ class PostActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_post)
+  }
+
+
+  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    menuInflater.inflate(R.menu.menu, menu)
+
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    when(item.getItemId()) {
+      R.id.search -> {
+        return true
+      }
+
+      else -> {
+        return super.onOptionsItemSelected(item)
+      }
+    }
   }
 }
