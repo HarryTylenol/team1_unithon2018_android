fe3c9a96a4ed5d0015f12f68113a127abd89682c
byungtak <>
2018-07-28 21:35:34 +0900

    adding post view

--- app/src/main/java/app/unithon2018/team1/team1unithon2018android/ui/activity/PostActivity.kt
+++ app/src/main/java/app/unithon2018/team1/team1unithon2018android/ui/activity/PostActivity.kt
@@ -8,14 +8,21 @@ import app.unithon2018.team1.team1unithon2018android.data.RoomRepository
 import app.unithon2018.team1.team1unithon2018android.network.ApiManager
 import android.widget.Toast
 import android.R.menu
+import android.support.v7.widget.LinearLayoutManager
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import app.unithon2018.team1.team1unithon2018android.ui.adapter.PostAdapter
+import kotlinx.android.synthetic.main.activity_post.*
 
 
 class PostActivity : AppCompatActivity() {
 
   private val apiService = ApiManager.getApiService()
+
+  private val postAdapter by lazy {
+    PostAdapter()
+  }
 
   private val roomRepository by lazy { RoomRepository.instance(apiService) }
   private val postRepository by lazy { PostRepository.instance(apiService) }
@@ -23,6 +30,15 @@ class PostActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_post)
+
+    with(post_recycler) {
+      layoutManager = LinearLayoutManager(this@PostActivity)
+      adapter = postAdapter
+    }
+
+    fab.setOnClickListener {
+
+    }
   }
 
 
